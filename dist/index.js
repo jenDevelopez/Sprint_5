9783b3b90b8fd9e12d1c6b3197b05421e8177ce5
jenDevelopez <jen1992lo@gmail.com>
2023-06-02 11:41:15 +0200

    Ejercicio 1 modificado

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,10 @@ const boton = document.getElementById("btn");
 const chiste = document.getElementById("chiste");
 const llamadaAPI = () => {
     fetch("https://icanhazdadjoke.com/slack")
-        .then(res => res.json())
+        .then((res) => res.json())
         .then(data => {
-        if (chiste instanceof HTMLElement) {
-            chiste.innerText = data.attachments[0].text;
-        }
+        console.log(data.attachments[0].fallback);
     })
-        .catch(err => console.error(err));
+        .catch((err) => console.error(err));
 };
 boton === null || boton === void 0 ? void 0 : boton.addEventListener("click", llamadaAPI);
--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,10 @@ const chiste = document.getElementById("chiste");
 const llamadaAPI = () => {
     fetch("https://icanhazdadjoke.com/slack")
         .then((res) => res.json())
-        .then((data) => {
-            if (chiste instanceof HTMLElement) {
-            chiste.innerText = data.attachments[0].text; 
-            }
+        .then(data => {
+            console.log(data.attachments[0].fallback) 
         })
         .catch((err) => console.error(err));
 };
 boton?.addEventListener("click", llamadaAPI);
+
