0bead4f4785889354fb6e509c1af8d3adb28ad98
jenDevelopez <jen1992lo@gmail.com>
2023-06-06 19:39:05 +0200

    Ejercicio 4 terminado

--- src/index.ts
+++ src/index.ts
@@ -45,3 +45,37 @@ function acudits(){
         .catch((err) => console.error(err));
 }
 buttonSubmit?.addEventListener("click",acudits)
+
+//NIVEL 2
+// EJERICIO 4
+function weather() {
+    const url = 'https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Barcelona&contentType=json&unitGroup=uk';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '47f62bf98cmsh1b21dc3a5988ea8p1f2b06jsn1398a8f6987a',
+            'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+        }
+    };
+
+    fetch(url,options)
+        .then(res => res.json())
+        .then(data => {
+            let container = document.querySelector("#weather")
+            let div:HTMLElement = document.createElement("div")
+            let info = data.locations["Barcelona"].values[0]
+            let nameCity = data.locations["Barcelona"].id
+            let city = document.createElement("p")
+            city.innerHTML = `${nameCity} `
+            city.classList.add("mx-3")
+            let temp = document.createElement("p")
+            temp.innerHTML = ` ${info.temp} ºC`
+            div.appendChild(city)
+            div.appendChild(temp)
+            div.classList.add("d-flex")
+            div.classList.add("justify-content-end")
+            container?.appendChild(div)
+        })
+        .catch(err => console.log(err))
+}
+document.addEventListener("DOMContentLoaded",weather);
